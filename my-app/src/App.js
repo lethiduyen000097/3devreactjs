0e698d1b85c3654a8e6f61549d62a5be0ca639bd
lethiduyen000097 <duyenlt060097@gmail.com>
2021-07-07 20:59:15 -0700

    checkbox row done!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,10 @@
 import React from "react";
 import Customers from "./component/customers.component";
-// import SingleRowSelectionGrid from "./component/rowSelection.component";
-// import QuickStart from "./component/quickStart.component";
-// import SimpleContainer from "./component/container.component";
-
-import EnhancedTable from "./component/checkboxTable.component";
 
 function App() {
   return (
     <div>
-      <EnhancedTable />
       <Customers />
-      {/* <SingleRowSelectionGrid /> */}
-      {/* <QuickStart /> */}
-      {/* <SimpleContainer /> */}
     </div>
     );
 }
--- my-app/src/component/customers.component.js
+++ my-app/src/component/customers.component.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { makeStyles, TableContainer, Checkbox, Table, TableHead, TableBody, TableRow, TableCell, Paper, Button } from '@material-ui/core';
+import React from 'react';
+import TablePagination from '@material-ui/core/TablePagination';
+import { makeStyles, TableContainer, Switch, FormControlLabel, Checkbox, Table, TableHead, TableBody, TableRow, TableCell, Paper, Button } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -17,23 +18,29 @@ const useStyles = makeStyles((theme) => ({
 const apiCustomers = [
   { id: 1, name: 'ABC', short_name: 'ADFD', tax_code: 13456, code: 'dfsg', address: 'dfdghjkl', status: true},
   { id: 2, name: 'AcC', short_name: 'ADFD', tax_code: 23456, code: 'dfsg', address: 'sdsdsfdghjkl', status: false},
-  { id: 3, name: 'ABb', short_name: 'ADFD', tax_code: 12356, code: 'dfsg', address: 'dhjkl', status: true}
+  { id: 3, name: 'ABb', short_name: 'ADFD', tax_code: 12356, code: 'dfsg', address: 'dhjkl', status: true},
+  { id: 4, name: 'AwC', short_name: 'ADFD', tax_code: 13456, code: 'dfsg', address: 'dfdghjkl', status: true},
+  { id: 5, name: 'ArC', short_name: 'ADFD', tax_code: 23456, code: 'dfsg', address: 'sdsdsfdghjkl', status: false},
+  { id: 6, name: 'Arb', short_name: 'ADFD', tax_code: 12356, code: 'dfsg', address: 'dhjkl', status: true},
+  { id: 7, name: 'AyC', short_name: 'ADFD', tax_code: 13456, code: 'dfsg', address: 'dfdghjkl', status: true},
+  { id: 8, name: 'AuC', short_name: 'ADFD', tax_code: 23456, code: 'dfsg', address: 'sdsdsfdghjkl', status: false},
+  { id: 9, name: 'Aib', short_name: 'ADFD', tax_code: 12356, code: 'dfsg', address: 'dhjkl', status: true},
+  { id: 10, name: 'AvC', short_name: 'ADFD', tax_code: 13456, code: 'dfsg', address: 'dfdghjkl', status: true},
+  { id: 11, name: 'AbC', short_name: 'ADFD', tax_code: 23456, code: 'dfsg', address: 'sdsdsfdghjkl', status: false},
+  { id: 12, name: 'Anb', short_name: 'ADFD', tax_code: 12356, code: 'dfsg', address: 'dhjkl', status: true},
+  { id: 13, name: 'AmC', short_name: 'ADFD', tax_code: 13456, code: 'dfsg', address: 'dfdghjkl', status: true},
+  { id: 14, name: 'AqC', short_name: 'ADFD', tax_code: 23456, code: 'dfsg', address: 'sdsdsfdghjkl', status: false},
+  { id: 15, name: 'Alb', short_name: 'ADFD', tax_code: 12356, code: 'dfsg', address: 'dhjkl', status: true}
   ];
 // End sample JSON
 
 const Customers = () => {
     const classes = useStyles();
 
-    const [customers, setCustomer] = useState(null);
     const [selected, setSelected] = React.useState([]);
-
-
-    const handleClickCheckboxInRow = (row) =>{
-
-      console.log(row);
-
-     
-    }
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [dense, setDense] = React.useState(false);
 
     const handleClick = (event, name) => {
       const selectedIndex = selected.indexOf(name);
@@ -55,9 +62,19 @@ const Customers = () => {
       setSelected(newSelected);
     };
 
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
 
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    };
 
- 
+    const handleChangeDense = (event) => {
+      setDense(event.target.checked);
+    };
+
     //render Group Buttons for Component 
     const _renderGroupButtons = () => {
 
@@ -71,6 +88,7 @@ const Customers = () => {
         </Button>
         <Button 
         variant="contained"
+        disabled
         >
           Disable
         </Button>
@@ -81,77 +99,96 @@ const Customers = () => {
 
 
   const isSelected = (name) => selected.indexOf(name) !== -1;
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, apiCustomers.length - page * rowsPerPage);
   // render Table to display invoices
   const _renderCustomerTable = () => {
     return (
-      <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">ID</TableCell>
-            <TableCell align="center">Name</TableCell>
-            <TableCell align="center">Shortname</TableCell>
-            <TableCell align="center">Tip code</TableCell>
-            <TableCell align="center">Code</TableCell>
-            <TableCell align="center">Address</TableCell>
-            <TableCell align="center">Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {apiCustomers.map((apiCustomers, index) => {
-            const isItemSelected = isSelected(apiCustomers.name);
-            const labelId = `enhanced-table-checkbox-${index}`;
-            return (
-<TableRow 
-            hover
-            onClick={(event) => handleClick(event, apiCustomers.name)}
-            role="checkbox"
-            aria-checked={isItemSelected}
-            tabIndex={-1}
-            key={apiCustomers.name}
-            selected={isItemSelected}
-
-            // key={apiCustomers.name}
-            // role="checkbox"
-            // onClick={(event) => handleClick(event, apiCustomers.name)}
-
-            >
-              <TableCell align="center">{apiCustomers.id}</TableCell>
-              <TableCell align="center">{apiCustomers.name}</TableCell>
-              <TableCell align="center">{apiCustomers.short_name}</TableCell>
-              <TableCell align="center">{apiCustomers.tax_code}</TableCell>
-              <TableCell align="center">{apiCustomers.code}</TableCell>
-              <TableCell align="center">{apiCustomers.address}</TableCell>
-              <TableCell padding="checkbox" align="center">
-                <Checkbox
-                  checked={isItemSelected}
-                  inputProps={{ 'aria-labelledby': labelId }}
-                  // checked={apiCustomers.status}
-                  // onClick = {handleClickCheckboxInRow.bind(this, apiCustomers)}
-                />
-                </TableCell>
-            </TableRow>
-            );
-            
-})}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    
-    )
-  }
+      <div className={classes.table}>
+        <Paper className={classes.paper}>
+        <TableContainer component={Paper}>
+          <Table 
+            className={classes.table} 
+            aria-label="simple table"
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">ID</TableCell>
+                <TableCell align="center">Name</TableCell>
+                <TableCell align="center">Shortname</TableCell>
+                <TableCell align="center">Tip code</TableCell>
+                <TableCell align="center">Code</TableCell>
+                <TableCell align="center">Address</TableCell>
+                <TableCell align="center">Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {apiCustomers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((apiCustomers, index) => {
+                const isItemSelected = isSelected(apiCustomers.name);
+                const labelId = `enhanced-table-checkbox-${index}`;
+                return (  
+                  <TableRow 
+                    hover
+                    onClick={(event) => handleClick(event, apiCustomers.name)}
+                    role="checkbox"
+                    aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={apiCustomers.name}
+                    selected={isItemSelected}
+                  >
+                    <TableCell align="center">{apiCustomers.id}</TableCell>
+                    <TableCell align="center">{apiCustomers.name}</TableCell>
+                    <TableCell align="center">{apiCustomers.short_name}</TableCell>
+                    <TableCell align="center">{apiCustomers.tax_code}</TableCell>
+                    <TableCell align="center">{apiCustomers.code}</TableCell>
+                    <TableCell align="center">{apiCustomers.address}</TableCell>
+                    <TableCell padding="checkbox" align="center">
+                      <Checkbox
+                        checked={isItemSelected}
+                        inputProps={{ 'aria-labelledby': labelId }}
+                      />
+                    </TableCell>
+                  </TableRow>
+                );       
+              })}
+              {emptyRows > 0 && (
+                    <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={apiCustomers.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+      control={<Switch checked={dense} onChange={handleChangeDense} />}
+      label="Dense padding"
+    />
+    </div>
+  )
+}
   // End render Table 
 
   // Main Render
-    return (
-        <div>
-            <h5>Customer table</h5>
-            <div style={{ height: 400, width: '100%' }}>
-              {_renderGroupButtons()}
-              {_renderCustomerTable()}           
-            </div>
-        </div>
-    )
+return (
+  <div>
+      <h5>Customer table</h5>
+      <div style={{ height: 400, width: '100%' }}>
+        {_renderGroupButtons()}
+        {_renderCustomerTable()}       
+      </div>
+  </div>
+  )
 }
 
 export default Customers;
